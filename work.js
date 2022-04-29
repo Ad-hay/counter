9891b928858f8b2b571b42857e9f0b703e0ca0a2
Ad-hay <99274118+Ad-hay@users.noreply.github.com>
2022-04-29 09:03:01 +0100

    Initial commit

---
+++ work.js
@@ -0,0 +1,11 @@
+//camelcase
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     countEl.innerHTML = count
+// }
